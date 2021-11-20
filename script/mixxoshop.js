f891693770ee5b6a6177a8b17af085cf7b534db2
haepppy <83610615+haepppy@users.noreply.github.com>
2021-11-20 15:57:36 +0900

    Update mixxoshop.js

--- script/mixxoshop.js
+++ script/mixxoshop.js
@@ -10,6 +10,20 @@ $(document).ready(function() {
     });
 
 
-    
+    //롤링 슬라이드 배너
+    let currentIndex = 0;
+    let slideCount = $('.bannerImg').length - 1;
+
+    setInterval(function() {
+        if(currentIndex < slideCount) {
+            currentIndex++;
+        } else {
+            currentIndex = 0;
+        }
+
+        let slidePosition = currentIndex * (-100) + 'vw';
+        $('.banner').animate({left: slidePosition}, 400);
+        console.log(slidePosition);
+    }, 3000);
 
 });
